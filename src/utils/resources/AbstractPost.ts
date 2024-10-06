f63ecb04bd8dda3558f02a4ac67419692d07ae29
rarula <74240663+rarula@users.noreply.github.com>
2024-10-06 13:16:20 +0900

    🚸 記事で使用できなかった記法などを使用可能に

--- src/utils/resources/AbstractPost.ts
+++ src/utils/resources/AbstractPost.ts
@@ -3,6 +3,7 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
+import remarkGfm from 'remark-gfm';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
 import rehypePrettyCode from 'rehype-pretty-code';
@@ -83,6 +84,7 @@ export abstract class AbstractPost<T extends AbstractPostMetadata> {
                 id: this._id,
                 type: this._type,
             })
+            .use(remarkGfm)
             .use(remarkRehype)
             .use(rehypePrettyCode, {
                 theme: 'one-dark-pro',
