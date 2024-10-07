88e768b630ca539c09154cf2cc68b7898f6b84ea
rarula <74240663+rarula@users.noreply.github.com>
2024-10-07 18:10:24 +0900

    🚸 ロゴ画像を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+import Image from 'next/image';
+
 const Home = (): JSX.Element => {
-    return <p className='text-center'>PAGE TOP</p>;
+    return (
+        <div className='pt-10 flex justify-center'>
+            <div className='w-80'>
+                <Image
+                    src='/forestotzka-logo.png'
+                    alt='Forestotzka Logo'
+                    priority={true}
+                    width={0}
+                    height={0}
+                    sizes='100vw'
+                    style={{ width: '100%', height: '100%' }}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Home;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
 
 import HamburgerMenuSvg from '@/components/svg/HamburgerMenuSvg';
 import { useHeaderMenu } from '@/contexts/HeaderMenu';
-import { Text } from '@/utils/Text';
 
 const Header = (): JSX.Element => {
     const { toggleOpen } = useHeaderMenu();
@@ -13,7 +13,17 @@ const Header = (): JSX.Element => {
         <header className='fixed w-full bg-green-700 shadow-xl z-50'>
             <div className='px-5 max-w-6xl mx-auto flex justify-between items-center h-20'>
                 <Link href='/'>
-                    <p className='text-xl text-white hover:underline underline-offset-4 decoration-1'>{Text.forestotzka}</p>
+                    <div className='w-36'>
+                        <Image
+                            src='/forestotzka-logo.png'
+                            alt='Forestotzka Logo'
+                            priority={true}
+                            width={0}
+                            height={0}
+                            sizes='100vw'
+                            style={{ width: '100%', height: '100%' }}
+                        />
+                    </div>
                 </Link>
                 <nav className='hidden lg:flex gap-10'>
                     <Link href='/news'>
